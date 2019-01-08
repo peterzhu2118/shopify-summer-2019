8266b1d38bd629305e8b703b0ef70f88a8ca7400
Peter Zhu <peter@peterzhu.ca>
2019-01-08 10:16:52 -0500

    Add Rubocop linting (#2)
    * Set up rubocop
    
    * Make rubocop happy
    
    * Install rubocop in CircleCI
    
    * Make binaries executable

--- db/migrate/20190108150310_create_cart_products.rb
+++ db/migrate/20190108150310_create_cart_products.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateCartProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :cart_products do |t|
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,41 +13,39 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_01_08_150310) do
-
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "cart_products", force: :cascade do |t|
-    t.bigint "cart_id"
-    t.bigint "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_cart_products_on_cart_id"
-    t.index ["product_id"], name: "index_cart_products_on_product_id"
+  create_table 'cart_products', force: :cascade do |t|
+    t.bigint 'cart_id'
+    t.bigint 'product_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['cart_id'], name: 'index_cart_products_on_cart_id'
+    t.index ['product_id'], name: 'index_cart_products_on_product_id'
   end
 
-  create_table "carts", force: :cascade do |t|
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_carts_on_user_id"
+  create_table 'carts', force: :cascade do |t|
+    t.bigint 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_carts_on_user_id'
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "title", null: false
-    t.decimal "price", precision: 11, scale: 2, null: false
-    t.integer "inventory_count", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_products_on_user_id"
+  create_table 'products', force: :cascade do |t|
+    t.string 'title', null: false
+    t.decimal 'price', precision: 11, scale: 2, null: false
+    t.integer 'inventory_count', null: false
+    t.bigint 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_products_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "token", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'username', null: false
+    t.string 'token', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
