bdd11dd24f437276ff730cc929cad130176025bc
Peter Zhu <peter@peterzhu.ca>
2019-01-09 18:44:37 -0500

    Root redirect (#9)
    * Add GraphQL docs
    
    * Add redirect from root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: redirect('https://github.com/peterzhu2118/shopify-summer-2019')
+
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post '/graphql', to: 'graphql#execute'
 end
