b8477b7130210f12a730fc29fd4d2dd98a9757f8
Peter Zhu <peter@peterzhu.ca>
2019-01-09 18:16:18 -0500

    Add query for all carts (#6)

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,9 +2,14 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :all_products, [ProductType], null: true
+    field :all_products, [ProductType], 'Get all the products.', null: true
     def all_products
       Product.all
+    end
+
+    field :all_carts, [CartType], 'Get all the carts that have not been purchased.', null: true
+    def all_carts
+      Cart.where(user: context[:current_user], purchased: false)
     end
   end
 end
