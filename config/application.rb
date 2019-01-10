c361cfd6abe926baab70647baef84a5f88483f10
Peter Zhu <peter@peterzhu.ca>
2019-01-09 21:57:46 -0500

    Set up Rack CORS (#11)
    * Set up Rack CORS
    
    * Makr rubocop happy

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,11 @@ module Marketplace
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i[get post put patch delete options head]
+      end
+    end
   end
 end
